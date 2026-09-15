97d7bc585a9c93ac4611ed8f5828aab9c44fca15
Ez <75935138+EzAuto399@users.noreply.github.com>
2026-09-15 21:53:04 +1000

    Redirect www visitors to the Veylet canonical domain

--- dist/brand-host.js
+++ dist/brand-host.js
@@ -1,4 +1,7 @@
 'use strict';
-if (location.hostname === 'veylet-site.vercel.app') {
+
+const nonCanonicalHosts = new Set(['veylet-site.vercel.app', 'www.veylet.com']);
+
+if (nonCanonicalHosts.has(location.hostname)) {
   location.replace('https://veylet.com' + location.pathname + location.search + location.hash);
 }
